a6c0a07426d56664504dc25861a468f082ee810a
Carlos <carlosrocha@mecajun.com>
2020-09-17 10:26:19 -0300

    connection alterations

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,18 @@
 import knex from 'knex'
+import 'dotenv/config'
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../../knexfile')
 const connection = knex(configuration[environment])
 
-export default connection
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : process.env.PG_HOST,
+        user : process.env.PG_USER,
+        password : process.env.PG_PASSWORD,
+        database : process.env.PG_DATABASE,
+    },
+    useNullAsDefault: true
+})
+
+export default db
