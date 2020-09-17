f24c9499ad8f3c1f39779b3d112893712a5dac06
Carlos <carlosrocha@mecajun.com>
2020-09-17 10:40:37 -0300

    fixing knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -2,30 +2,18 @@ import path from 'path'
 import 'dotenv/config'
 
 module.exports = {
-    development:{
-        client: 'sqlite3',
-        connection: {
-            filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
-        },
-        migrations:{
-            directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
-        },
-        seeds:{
-            directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
-        },
-        useNullAsDefault: true
+    client: 'pg',
+    connection: {
+        host : process.env.PG_HOST,
+        user : process.env.PG_USER,
+        password : process.env.PG_PASSWORD,
+        database : process.env.PG_DATABASE,
     },
-    production:{
-        client: 'pg',
-        connection: {
-            host : process.env.PG_HOST,
-            user : process.env.PG_USER,
-            password : process.env.PG_PASSWORD,
-            database : process.env.PG_DATABASE,
-        },
-        migrations: {
-            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-        },
-        useNullAsDefault: true
-    }
+    migrations: {
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    seeds: {
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+    },
+    useNullAsDefault: true
 }
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,5 @@
 import knex from 'knex'
 import 'dotenv/config'
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('../../knexfile')
-const connection = knex(configuration[environment])
 
 const db = knex({
     client: 'pg',
